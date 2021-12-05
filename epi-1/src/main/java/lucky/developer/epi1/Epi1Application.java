dc136d193b18aed3eff5ae927faa8b5b6a71dc66
hwanseok <hwanseok@whatap.io>
2021-12-05 09:35:21 +0900

    add epi-1

---
+++ epi-1/src/main/java/lucky/developer/epi1/Epi1Application.java
@@ -0,0 +1,13 @@
+package lucky.developer.epi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Epi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Epi1Application.class, args);
+	}
+
+}
